e4e71e1b1b4213615ecf2cad741e5544c5a9f496
18Pixels(I) Pvt. Ltd <rk468335@gmail.com>
2021-09-28 15:44:07 +0530

    model and schema updated

--- books/models.py
+++ books/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.fields import DateTimeField
+from django.db.models.fields.related import ForeignKey
+from django.utils.translation import gettext_lazy
 
 # Create your models here.
 class Books(models.Model):
@@ -8,3 +11,52 @@ class Books(models.Model):
 
     def __str__(self):
         return self.title
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    timeStamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Quizzes(models.Model):
+    title = models.CharField(max_length=100,default="New Quiz")
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    timeStamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    SCALE = (
+        (0,'Fundamental'),
+        (1,'Beginner'),
+        (2,'Intermediate'),
+        (3,'Advanced'),
+        (4,'Expert'),
+    )
+
+    TYPE = (
+        (0,'Multiple Choice'),
+    )
+
+    quiz = models.ForeignKey(Quizzes,on_delete=models.CASCADE)
+    technique = models.IntegerField(choices=TYPE,default=0,verbose_name="Type of Question")
+    title = models.CharField(max_length=50,verbose_name="Question")
+    difficulty = models.IntegerField(choices=SCALE,default=0,verbose_name="Difficulty Level")
+    timeStamp = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=False,verbose_name='Active Status')
+
+    def __str__(self):
+        return self.title
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, related_name='answer', on_delete=models.CASCADE)
+    answerText = models.CharField(max_length=200,verbose_name="Answer Text")
+    is_right = models.BooleanField(default=False)
+    timeStamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.answerText
--- books/schema.py
+++ books/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Books
+from graphene_django import DjangoListField
+from .models import *
 
 
 
@@ -9,15 +10,47 @@ class BookType(DjangoObjectType):
         model = Books
         fields = ('id','title','excerpt','timeStamp')
 
-# Query >>> for fetching data
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ('id','name','timeStamp')
+
+class QuizzesType(DjangoObjectType):
+    class Meta:
+        model = Quizzes
+        fields = ('id','title','category','timeStamp')
+
+
+class QuestionType(DjangoObjectType):
+    class Meta:
+        model = Question
+        fields = ('id','technique','title','difficulty','is_active','timeStamp')
+
+
+class AnswerType(DjangoObjectType):
+    class Meta:
+        model = Answer
+        fields = ('id','question','answerText','timeStamp')
+
+
+# ============================================================================
+'''Query >>> for fetching data'''
+# ============================================================================
 class Query(graphene.ObjectType):
     all_books = graphene.List(BookType)
 
     def resolve_all_books(root,info):
         return Books.objects.all()
         # return Books.objects.filter(title__startswith='God')
+# ----------------------------------------------------------------------------
+class Query(graphene.ObjectType):
+    all_category = graphene.List(CategoryType)
 
-
-
+    def resolve_all_category(root,info):
+        return Category.objects.all()
+# ============================================================================
+'''SCHEMA TO ACCESS MODEL DATA'''
+# ============================================================================
 schema = graphene.Schema(query=Query)
 
